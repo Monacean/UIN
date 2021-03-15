48b8100d8e7422a4895530345ee003ca6557ca57
Monacean <70483631+Monacean@users.noreply.github.com>
2021-03-15 19:11:31 +0100

    Endret navn til movies
    leste feil i oppgaveteksten.

--- oblig3/monacasanity/src/App.js
+++ oblig3/monacasanity/src/App.js
@@ -1,10 +1,10 @@
-import Movie from './components/Movie.js';
+import Movies from './components/Movies.js';
 
 const App = () => {
   return (
   <>
     <h1>Things will happen here</h1>
-    <Movie />
+    <Movies />
   </>
 );
 };
